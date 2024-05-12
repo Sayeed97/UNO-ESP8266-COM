a6895b823ad0d67ce1103d43b942e4f8a22c5ecc
charantejaabbavathini <charantejafeb26@gmail.com>
2024-05-12 10:08:57 -0400

    I2C Device Scanner

---
+++ I2C_device_scanner.ino
@@ -0,0 +1,47 @@
+// I2C address finding
+#include <Wire.h>
+
+void setup()
+{
+	//Initializing wire
+	Wire.begin();
+	//Initializing seraial monitor at the baudrate of 9600
+	Serial.begin(9600);
+}
+
+void loop()
+{
+	byte err, addr;
+	//Declaring variable to detect and count no. of I2C device found
+	int devices = 0;
+	
+	// For loop to try multiple combinations of Address
+	for (addr = 1; addr < 127; addr++) 
+	{
+		Wire.beginTransmission(addr);
+		err = Wire.endTransmission();
+
+    // err = 0 for a successful transmission end
+    // !0 == true; which means we have a successfully found a device in the provided address
+		if (!err) 
+		{
+			Serial.print("Device address: 0x");
+			Serial.println(addr, HEX);
+			devices++;
+		}
+		else if (err == 4) // unknown error from the I2C device
+		{
+			Serial.print("Error at address: 0x");
+			Serial.println(addr, HEX);
+		}
+	}
+	
+	//Exception, when there is no I2C device found
+	if (!devices)
+	{
+	Serial.println("Please connect your I2C device");
+	}
+	
+	//Waiting for 2 seconds
+	delay(2000);
+}
